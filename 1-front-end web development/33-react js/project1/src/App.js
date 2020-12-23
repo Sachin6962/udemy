f8594a3f22e4c4d5d4eabcffc635cd11a83ec314
Sachin6962 <setenergy6962@gmail.com>
2020-12-23 15:29:54 +0100

    update 23dec2020

---
+++ 1-front-end web development/33-react js/project1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Heeey</h1>
+      <ul>
+        <li>Burger King</li>
+        <li>Chinese winkel</li>
+        <li>Resto snackbar</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
