f380a312f3934b1b927be01c309a6363e618bfa9
Sachin6962 <setenergy6962@gmail.com>
2020-12-24 15:32:15 +0100

    update 24dec2020

--- 1-front-end web development/33-react js/project1/src/App.js
+++ 1-front-end web development/33-react js/project1/src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
 
+var tijd = new Date().getHours();
+var uitspraken = {
+  ochtend   : "Goeiemorgen",
+  middag    : "Goedemiddag",
+  avond     : "Goedeavond"
+}
+var kleur = {
+  color     : ""
+}
+
+function bereken() {
+  if (tijd >= 0 && tijd < 12) {
+    kleur.color = "red";
+    return uitspraken.ochtend;
+  }
+  else if (tijd >= 12 && tijd < 18) {
+    kleur.color = "green";
+    return uitspraken.middag;
+  }
+  else {
+    kleur.color = "blue";
+    return uitspraken.avond;
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Heeey</h1>
-      <ul>
-        <li>Burger King</li>
-        <li>Chinese winkel</li>
-        <li>Resto snackbar</li>
-      </ul>
+      <h1 className="heading" style={kleur}>{bereken()}</h1>
     </div>
   );
 }
