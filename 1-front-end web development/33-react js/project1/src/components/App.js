fb669b75324e127226bf46e1147a9608597591dd
Sachin6962 <setenergy6962@gmail.com>
2020-12-24 16:14:36 +0100

    update 24dec2020

---
+++ 1-front-end web development/33-react js/project1/src/components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import Lijst from './Lijst';
+
+function App() {
+  return (
+    <div className="App">
+      <Lijst />
+      <Lijst />
+    </div>
+  );
+}
+
+export default App;
---
+++ 1-front-end web development/33-react js/project1/src/components/Lijst.js
@@ -0,0 +1,16 @@
+function Lijst() {
+  return (
+    <div>
+    <h1 className="heading">Burger king bestellen</h1>
+      <ul>
+        <li>Big King</li>
+        <li>Long Chicken</li>
+        <li>Patat</li>
+        <li>Cola</li>
+      </ul>
+    </div>
+  
+  )
+}
+
+export default Lijst;
