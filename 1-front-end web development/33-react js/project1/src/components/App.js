adb0cf22613aad5993976b97401a41dbe4f2e184
Sachin6962 <setenergy6962@gmail.com>
2021-01-01 15:41:53 +0100

    update 1jan2020

--- 1-front-end web development/33-react js/project1/src/components/App.js
+++ 1-front-end web development/33-react js/project1/src/components/App.js
@@ -1,11 +1,18 @@
-import '../App.css';
-import Lijst from './Lijst';
+function Kaarten(props) {
+  return (
+    <div className='kaarten'>
+      <h2>{props.naam}</h2>
+      <img src={props.foto}></img>
+      <p>{props.beschrijving}</p>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <Lijst />
-      <Lijst />
+    <div>
+      <Kaarten naam='Brad Traversy' foto='https://i0.wp.com/trydiscountcoupons.com/wp-content/uploads/2017/10/brad-traversy-instructor-udemy.jpg?fit=200%2C200&ssl=1' beschrijving='web development man'/>
+      <Kaarten naam='David Goggins' foto='https://pbs.twimg.com/profile_images/1277635237317574657/1W7y6V4a.jpg' beschrijving='overwin jezelf'/>
     </div>
   );
 }
